72adf6cb7d03f559509c1c4d1aa8e0d51dfeaaae
Luis Blunschi <lublu@hotmail.ch>
2025-12-01 16:21:23 +0000

    Preparing to Merge

--- path_planner/Mockup_Snake_planner.py
+++ path_planner/Mockup_Snake_planner.py
@@ -115,16 +115,19 @@ def set_yaw(yaw_deg, speed_deg_per_s=10, relative=False):
     else:
         print("No ACK for yaw")
 
-def goto_position(lat, lon, depth, yaw_deg=None):
-    master.mav.set_position_target_global_int_send(
+def goto_position(x_east, y_north, z_down, yaw_deg=None):
+    x_north = y_north
+    y_east = x_east
+
+    master.mav.set_position_target_local_ned_send(
         0,
-        master.target_system,
-        master.target_component,
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+        int(master.target_system),
+        int(master.target_component),
+        int(mavutil.mavlink.MAV_FRAME_LOCAL_NED),
         int(0b110111111000),  # use position only
-        int(lat*1e7),
-        int(lon*1e7),
-        -depth,
+        x_north,
+        y_east,
+        z_down,
         0, 0, 0,
         0, 0, 0,
         math.radians(yaw_deg) if yaw_deg is not None else 0,
@@ -132,38 +135,38 @@ def goto_position(lat, lon, depth, yaw_deg=None):
     )
     if yaw_deg is not None:
         set_yaw(yaw_deg)
-
+    print(f"Sending local position: X (Norht): {x_north}, Y (East): {y_east}, Z (Down): {z_down}")
     ack = master.recv_match(type='COMMAND_ACK', blocking=True, timeout=5)
     if ack:
         print(f"Position ACK: {ack.result}")
     else:
         print("No ACK for position")
 
-def reached_goal(lat_goal, lon_goal, depth_goal, yaw_goal, threshold=0.05):
+def reached_goal(x_east_goal, y_north_goal, z_down_goal, yaw_goal, threshold=0.05):
     """
     Check if current position is within threshold of target
     threshold in meters
     """
-    pos_msg = master.recv_match(type='GLOBAL_POSITION_INT', blocking=True, timeout=1)
+    pos_msg = master.recv_match(type='LOCAL_POSITION_NED', blocking=True, timeout=1)
     yaw_msg = master.recv_match(type='ATTITUDE', blocking=True, timeout=1)
     #print(yaw_msg)
     if not pos_msg:
         return False, None
     
-    lat_current = pos_msg.lat / 1e7
-    lon_current = pos_msg.lon / 1e7
-    depth_current = -pos_msg.relative_alt / 1000 - 49.35 # convert mm to m
+    x_east_current = pos_msg.y
+    y_north_current = pos_msg.x
+    z_down_current = pos_msg.z
 
     # Approximate distance in meters using simple latitude/longitude differences
-    lat_dist = (lat_goal - lat_current) * 111000
-    lon_dist = (lon_goal - lon_current) * 111000 * math.cos(math.radians(lat_goal))
-    depth_dist = depth_goal - depth_current
+    x_dist = (x_east_goal - x_east_current)
+    y_dist = (y_north_goal - y_north_current)
+    depth_dist = z_down_goal - z_down_current
     if yaw_msg:
         #print(yaw_msg)
         yaw_dist = yaw_goal - yaw_msg.yaw*360/2/math.pi 
     else:
         yaw_dist = 0
-    total_dist = math.sqrt(lat_dist**2 + lon_dist**2 + depth_dist**2 + yaw_dist**2/100)
+    total_dist = math.sqrt(x_dist**2 + y_dist**2 + depth_dist**2 + yaw_dist**2/100)
 
     # Get yaw for logging
     att_msg = master.recv_match(type='ATTITUDE', blocking=False)
@@ -171,7 +174,7 @@ def reached_goal(lat_goal, lon_goal, depth_goal, yaw_goal, threshold=0.05):
 
     # Print safely even if yaw is None
     yaw_str = f"{yaw_deg:.1f}°" if yaw_deg is not None else "N/A"
-    print(f"Current: dist_lat={lat_dist:.7f}, dist_lon={lon_dist:.7f}, dist_depth={depth_dist:.2f} m, dist_yaw={yaw_dist}, total_dist={total_dist:.2f} m")
+    print(f"Current: dist_x={x_dist:.7f}, dist_y={y_dist:.7f}, dist_depth={depth_dist:.2f} m, dist_yaw={yaw_dist}, total_dist={total_dist:.2f} m")
 
     return total_dist < threshold, total_dist
 
@@ -192,10 +195,10 @@ top_right = {"lat": 41.3585, "lon": 2.185416667,    "depth": 3.75, "yaw": 105.69
 bottom_left = {"lat": 41.358508333, "lon": 2.185419444, "depth": 5.15,  "yaw": 105.6923}
 bottom_right = {"lat": 41.3585, "lon": 2.185416667,   "depth": 5.15,  "yaw": 105.6923}
 
-top_left = {"lat":41.358510425, "lon": 2.185408384, "depth": 3.75, "yaw": 105.6923}
-top_right = {"lat": 41.358502092, "lon": 2.185405607,    "depth": 3.75, "yaw": 105.6923}
-bottom_left = {"lat": 41.358510425, "lon": 2.185408384, "depth": 5.15,  "yaw": 105.6923}
-bottom_right = {"lat": 41.358502092, "lon": 2.185405607,   "depth": 5.15,  "yaw": 105.6923}
+top_left = {"x":10.92, "y": 13.55, "depth": 3.75, "yaw": 105.6923}
+top_right = {"x": 10.7, "y": 12.63,    "depth": 3.75, "yaw": 105.6923}
+bottom_left = {"x": 10.92, "y": 13.56, "depth": 5.15,  "yaw": 105.6923}
+bottom_right = {"x": 10.7, "y": 12.63,   "depth": 5.15,  "yaw": 105.6923}
 
 depth_step = 0.2
 current_depth = top_left["depth"]
@@ -221,37 +224,37 @@ while not done:
     home = master.recv_match(type='HOME_POSITION', blocking=True, timeout=5)
     if not checked_apriltags:
         print("Checking for AprilTags before starting snake path...")
-        lat_end = bottom_left["lat"]
-        lon_end = bottom_left["lon"]
+        x_end = bottom_left["x"]
+        y_end = bottom_left["y"]
         depth_end = bottom_left["depth"]
-        goto_position(lat_end, lon_end, depth_end+0.05, yaw_deg=yaw)
+        goto_position(x_end, y_end, depth_end, yaw_deg=yaw)
         while not checked_apriltags:
-            checked_apriltags, dist = reached_goal(lat_end, lon_end, depth_end+0.05, yaw, threshold=0.1)
+            checked_apriltags, dist = reached_goal(x_end, y_end, depth_end, yaw, threshold=0.05)
             time.sleep(0.5)
 
     if going_right:
-        lat_start = top_left["lat"]
-        lat_end = top_right["lat"]
-        lon_start = top_left["lon"]
-        lon_end = top_right["lon"]
+        x_start = top_left["x"]
+        x_end = top_right["x"]
+        y_start = top_left["y"]
+        y_end = top_right["y"]
     else:
-        lat_start = top_right["lat"]
-        lat_end = top_left["lat"]
-        lon_start = top_right["lon"]
-        lon_end = top_left["lon"]
+        x_start = top_right["x"]
+        x_end = top_left["x"]
+        y_start = top_right["y"]
+        y_end = top_left["y"]
 
     # Move to start of line
-    goto_position(lat_start, lon_start, current_depth, yaw_deg=yaw)
+    goto_position(x_start, y_start, current_depth, yaw_deg=yaw)
     reached = False
     while not reached:
-        reached, dist = reached_goal(lat_start, lon_start, current_depth, yaw, threshold=0.05)
+        reached, dist = reached_goal(x_start, y_start, current_depth, yaw, threshold=0.05)
         time.sleep(0.5)
 
     # Move to end of line
-    goto_position(lat_end, lon_end, current_depth, yaw_deg=yaw)
+    goto_position(x_end, y_end, current_depth, yaw_deg=yaw)
     reached = False
     while not reached:
-        reached, dist = reached_goal(lat_end, lon_end, current_depth, yaw, threshold=0.05)
+        reached, dist = reached_goal(x_end, y_end, current_depth, yaw, threshold=0.05)
         time.sleep(0.5)
 
     # Step down
