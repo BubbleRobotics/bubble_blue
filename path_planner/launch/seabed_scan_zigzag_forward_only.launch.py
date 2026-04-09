13d5e972393359d3017d0057654a38a62e4027b3
Aleedm <alessandro@bubble-robotics.com>
2026-04-09 11:39:10 +0200

    More fancy path :D

---
+++ path_planner/launch/seabed_scan_zigzag_forward_only.launch.py
@@ -0,0 +1,35 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    default_config = os.path.join(
+        get_package_share_directory("path_planner"),
+        "config",
+        "seabed_scan_zigzag_forward_only.yaml",
+    )
+
+    params_file = LaunchConfiguration("params_file")
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "params_file",
+                default_value=default_config,
+                description="YAML config for the zig-zag forward-only seabed scan planner",
+            ),
+            Node(
+                package="path_planner",
+                executable="seabed_scan_planner_zig_zag_forward_only",
+                name="seabed_scan_planner_zig_zag_forward_only",
+                output="screen",
+                parameters=[
+                    params_file,
+                ],
+            ),
+        ]
+    )
