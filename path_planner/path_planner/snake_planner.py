8cc558d2e9036089929b84048019a6facc7a24c7
Luis Blunschi <lublu@hotmail.ch>
2026-05-06 17:22:28 +0000

    Adjusted depth step to match barcelona trials

--- path_planner/path_planner/snake_planner.py
+++ path_planner/path_planner/snake_planner.py
@@ -270,7 +270,7 @@ class BodyPIDFollower(Node):
         self.bottom_left = {"x": 0.0, "y": 0.0, "depth": 1.0}
         self.bottom_right = {"x": x_right, "y": y_right, "depth": 1.0}
 
-        self.depth_step = 0.2
+        self.depth_step = 0.15
         self.current_depth = self.top_left["depth"]
         self.max_depth = self.bottom_left["depth"]
         self.going_right = True
